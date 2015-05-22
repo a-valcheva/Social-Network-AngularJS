8085c5734a99132d9198d447dab58414f00164b8
anichka777 <avalcheva7@gmail.com>
2015-05-22 20:48:57 +0300

    part of routing

--- app/js/app.js
+++ app/js/app.js
@@ -5,8 +5,43 @@ var socialNetworkApp = angular.module('socialNetworkApp', [])
     .config(function($routeProvider){
         $routeProvider
             .when('/', {
-                templateUrl: 'templates/user/user-home-screen.html',
-                controller: 'LoadAllPosts'
+                templateUrl: 'templates/public/home.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'templates/public/login.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/register', {
+                templateUrl: 'templates/public/register.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/users/:username', {
+                templateUrl: 'templates/user/user-wall.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/users/:username/friends', {
+                templateUrl: 'templates/user/user-wall.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/profile', {
+                templateUrl: 'templates/user/user-wall.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/profile/password', {
+                templateUrl: 'templates/user/user-wall.html'
+                //controller: 'LoadAllPosts'
+            });
+        $routeProvider
+            .when('/logout', {
+                templateUrl: 'templates/user/user-wall.html'
+                //controller: 'LoadAllPosts'
             });
             //.otherwise({ redirectTo: '/customers' });
     });
